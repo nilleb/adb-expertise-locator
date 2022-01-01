a9365fad8c4d55947cb50f54cbddb63345548641
Ivo Bellin Salarin <me@nilleb.com>
2022-01-01 10:33:29 +0100

    feat: use another autocompletion component

--- ui/src/components/SearchPage.vue
+++ ui/src/components/SearchPage.vue
@@ -2,28 +2,41 @@
   <div id="searchPage">
     <h1>Portable Search Engine</h1>
     <div>
-      <Autocomplete
+      <SimpleTypeahead
+					:items="suggestions"
+					:placeholder="options.placeholder"
+					@selectItem="selectItem"
+					@onInput="onInput"
+					@onBlur="onBlur"
+					:minInputLength="options.minInputLength"
+					:itemProjection="
+						(item) => {
+							return item.title || item;
+						}
+					"
+				/>
+      <!--Autocomplete
         :debounce=200
         placeholder="Type anything..."
         @input="getItems"
         :results="suggestions"
         style="width: 80%; margin: 0 auto;"
-      ></Autocomplete>
+      ></Autocomplete-->
     </div>
     <SearchResults :searchResults="searchResults" />
   </div>
 </template>
 
 <script>
-import Autocomplete from "vue3-autocomplete";
-import "vue3-autocomplete/dist/vue3-autocomplete.css";
+import SimpleTypeahead from 'vue3-simple-typeahead';
+import 'vue3-simple-typeahead/dist/vue3-simple-typeahead.css'; //Optional default CSS
 import KnowledgeService from "../services/KnowledgeService";
 import SearchResults from "./SearchResults.vue";
 
 export default {
   name: "SearchPage",
   components: {
-    Autocomplete,
+    SimpleTypeahead,
     SearchResults,
   },
   props: {
@@ -31,17 +44,39 @@ export default {
   },
   data() {
     return {
+			options: {
+				placeholder: 'Type anything...',
+				minInputLength: 1,
+			},
       query: "",
       suggestions: [],
       searchResults: [],
+      data: {
+				input: '',
+				selection: null,
+			},
     };
   },
   methods: {
+    selectItem(item) {
+      console.log(event);
+			this.data.selection = item;
+		},
+		onInput(event) {
+			this.data.selection = null;
+			this.data.input = event.input;
+      this.getItems(event.input);
+			this.listFiltered = event.items;
+		},
+		onBlur(event) {
+			this.data.input = event.input;
+			this.listFiltered = event.items;
+		},
     getItems(string) {
       KnowledgeService.search(string).then((response) => {
         this.suggestions = response.suggestions;
         this.searchResults = response.results;
-        console.log(this.searchResults);
+        console.log(response);
       });
     },
   },
@@ -60,5 +95,8 @@ h1 {
   text-align: left;
   color: #2c3e50;
   margin-top: 60px;
+}
+.demo:deep() .simple-typeahead-list {
+	max-height: 200px !important;
 }
 </style>
