73acbb2d36e93a8edaab8c5d9bf0251b3f6cb7c6
Ivo Bellin Salarin <me@nilleb.com>
2022-01-11 23:54:21 +0100

    feat: sample nginx conf

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,8 @@ mkdir /tmp/app/data
 cp -R data/intermediate /tmp/app/data
 cp deploy/.buildpacks /tmp/app
 cp deploy/Procfile /tmp/app
-cd /tmp/
+cp deploy/nginx.conf /tmp/app
+pushd /tmp/
 rm -rf /tmp/app/node_modules
 
 tar czvf app.tar.gz --exclude='app/data/input' --exclude='app/.git' --exclude='app/.vscode' --exclude='app/venv' --exclude='app/node_modules' --exclude='app/__pycache__' --exclude='app/backends/__pycache__' --exclude='app/configuration/__pycache__' --exclude='app/.DS_Store' app
