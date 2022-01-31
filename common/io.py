1b0e3b189730aacf3828b01025b3bb803dc4ec57
Ivo Bellin Salarin <me@nilleb.com>
2022-01-31 22:36:02 +0100

    feat: a default encoder

--- common/io.py
+++ common/io.py
@@ -1,9 +1,17 @@
 import json
+from json import JSONEncoder
+
 
 def read_object(path):
     with open(path) as fd:
         return json.load(fd)
 
-def write_object(obj, path):
-    with open(path, 'w') as fd:
-        json.dump(obj, fd)
+
+def write_object(obj, path, cls=None):
+    with open(path, "w") as fd:
+        json.dump(obj, fd, cls=cls)
+
+
+class MyEncoder(JSONEncoder):
+    def default(self, o):
+        return o.__dict__
