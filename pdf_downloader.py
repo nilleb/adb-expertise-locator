47125dc2edc276d912f3c1ac203b0b0ea58b598d
Ivo Bellin Salarin <me@nilleb.com>
2022-01-31 11:51:12 +0100

    feat: filtered download

--- pdf_downloader.py
+++ pdf_downloader.py
@@ -1,23 +1,26 @@
 import json
 import os
 from urllib.parse import urlparse
+import logging
 
 import requests
 from requests.exceptions import ChunkedEncodingError, SSLError
 
+logging.basicConfig(level=logging.INFO)
+
 
 def build_sets():
-    sets = {}
-
-    if os.path.exists('data/intermediate/sets.json'):
-        with open('data/intermediate/sets.json') as fd:
+    try:
+        with open("data/intermediate/sets.json") as fd:
             sets = json.load(fd)
 
-    if not sets:
-        filenames = ['pdf-generic', 'technical', 'reports']
+    except:
+        sets = {}
+        filenames = ["pdf-generic", "technical", "reports"]
+
         for filename in filenames:
-            if os.path.exists(f'{filename}.txt'):
-                print(f"{filename}.txt")
+            if os.path.exists(f"{filename}.txt"):
+                logging.info(f"processing {filename}.txt")
                 with open(f"{filename}.txt") as fd:
                     current = set()
                     for line in fd:
@@ -32,8 +35,19 @@ def build_sets():
 
 def get_filename(url):
     fn = os.path.basename(urlparse(url).path)
-    fn = fn if fn.endswith('.pdf') else f"{fn}.pdf"
+    fn = fn if fn.endswith(".pdf") else f"{fn}.pdf"
     return fn
+
+
+def download_file(url, path):
+    try:
+        response = requests.get(url)
+    except (ChunkedEncodingError, SSLError):
+        logging.error(f"error caught downloading {url}")
+        yield url
+
+    with open(path, "wb") as fd:
+        fd.write(response.content)
 
 
 def download_item_set(folder, urls):
@@ -41,21 +55,17 @@ def download_item_set(folder, urls):
     total, count = len(urls), 0
     for url in urls:
         count += 1
-        path = f'{folder}/{get_filename(url)}'
-        print(f"({count}/{total}) -> {path} (exists? {os.path.exists(path)})")
+        path = f"data/input/{folder}/{get_filename(url)}"
+        logging.info(f"({count}/{total}) -> {path} (exists? {os.path.exists(path)})")
         if not os.path.exists(path):
-            try:
-                response = requests.get(url)
-            except (ChunkedEncodingError, SSLError):
-                errors.append(url)
-                print(f"error caught downloading {url}")
-                continue
-            with open(path, 'wb') as f:
-                f.write(response.content)
+            to_retry = list(download_file(url, path))
+            if to_retry:
+                errors.extend(to_retry)
     return errors
 
+
 def download_and_retry_once(key, urls):
-    print(f"downloading {key}")
+    logging.info(f"downloading {key} ({len(urls)} urls)")
     if not os.path.exists(key):
         os.mkdir(key)
     download_set = urls
@@ -64,7 +74,13 @@ def download_and_retry_once(key, urls):
         download_set = list(set(download_set_post) - set(download_set))
 
 
-if __name__ == '__main__':
+def filter_urls(urls):
+    for url in urls:
+        if "rrp" in url:
+            yield url
+
+
+if __name__ == "__main__":
     sets = build_sets()
     for key, urls in sets.items():
-        download_and_retry_once(key, urls)
+        download_and_retry_once(key, list(filter_urls(urls)))
