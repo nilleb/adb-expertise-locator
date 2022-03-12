69c15bfc9b64d6c8e398e1a0351c95a2eaec4bd1
Ivo Bellin Salarin <me@nilleb.com>
2022-03-12 08:29:06 +0100

    feat(profile): retrieve information on mount

--- ui/src/components/Profile.vue
+++ ui/src/components/Profile.vue
@@ -1,7 +1,12 @@
 <template>
   <div>
-    <avatar background="lightgray" color="black" class="topright" :name="`${firstName} ${lastName}`"></avatar>
-    <h1>Hey, {{ firstName }}!</h1>
+    <avatar
+      background="lightgray"
+      color="black"
+      class="topright"
+      :name="`${user.firstName} ${user.lastName}`"
+    ></avatar>
+    <h1>Hey, {{ user.firstName }}!</h1>
 
     <h2>Your skills</h2>
     <vue-tags-input
@@ -15,7 +20,38 @@
       placeholder="Add a skill name - press enter when complete."
     />
     <h2>What do we know about you?</h2>
-    ...
+    <table>
+      <tr>
+        <th>fullname</th>
+        <td>{{ user.fullname }}</td>
+      </tr>
+      <tr>
+        <th>email</th>
+        <td>{{ user.email }}</td>
+      </tr>
+      <tr>
+        <th>phone</th>
+        <td>{{ user.telephoneNumber }}</td>
+      </tr>
+      <tr>
+        <th>role</th>
+        <td>{{ user.role }}</td>
+      </tr>
+      <tr>
+        <th>dept</th>
+        <td>{{ user.organization }}</td>
+      </tr>
+    </table>
+    <h3>Documents you have worked on</h3>
+    <span v-for="document in user.documents" :key="document.path">
+      <a :href="document.url">{{ document.title }}</a> 💠
+    </span>
+    <h3>Keywords</h3>
+    <span v-for="keyword in user.keywords" :key="keyword.keyword">
+      <i>{{ keyword.keyword }}</i> ({{ keyword.count }}),
+    </span>
+    <h3>Feedback</h3>
+    <a :href="'/edit?' + uid">signal an error</a>
   </div>
 </template>
 
@@ -34,9 +70,12 @@ export default {
       tag: "",
       tags: [],
       autocompleteItems: [],
-      userEmail: "ivo@nilleb.com",
-      firstName: "Ivo",
-      lastName: "Bellin Salarin"
+      uid: "ivo-bellin-salarin",
+      user: {
+        email: "ivo@nilleb.com",
+        firstName: "Ivo",
+        lastName: "Bellin Salarin",
+      },
     };
   },
   watch: {
@@ -72,12 +111,29 @@ export default {
         /* expects: list of items with a .text property */
       });
     },
+    getItem(uid) {
+      KnowledgeService.getDocument(uid).then((response) => {
+        const source = JSON.parse(response.source);
+        this.user = source;
+        this.user.firstName = source.fullname.split(" ")[0];
+        this.user.lastName = source.fullname.split(" ")[-1];
+        this.tags = [{ text: "tag1" }, { text: "tag2" }];
+      });
+    },
+  },
+  mounted() {
+    if (this.$route.query.uid) {
+      this.$data.uid = this.$route.query.uid;
+    }
+    if (this.$data.uid) {
+      this.getItem(this.$data.uid);
+    }
   },
 };
 </script>
 
 <style scoped>
 .topright {
-    float: right;
+  float: right;
 }
 </style>
